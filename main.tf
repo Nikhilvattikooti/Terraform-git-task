d0873351129262c9005464041002df8ecbea4e46
Nikhilvattikooti <133952673+Nikhilvattikooti@users.noreply.github.com>
2023-06-07 16:34:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -82,7 +82,7 @@ resource "aws_route_table" "prod-private-crt" {
 
 resource "aws_route_table_association" "a" {
   subnet_id      = aws_subnet.subnet-1.id
-  subnet_id      = aws_subnet.subnet-3.id
+  //subnet_id      = aws_subnet.subnet-3.id
   route_table_id = aws_route_table.prod-public-crt.id
 }
 
